eabe92e077c601b66f302f0226b204e1f27295be
DaVinci103 <58339513+DaVinci103@users.noreply.github.com>
2024-04-29 11:35:09 +0200

    Update gms.js
    some debugging

--- gms.js
+++ gms.js
@@ -31,7 +31,7 @@ function calc() {
     let val = $("#M").val();
     let M = val.slice(1, val.length - 1).split(")(").map(col => col.split(",").map(x => Number(x)));
     let n = Number($("#n").val());
-    $("#expanded").text(GMS(M, n).map(col => "(" + col.join(",") + ")"));
+    $("#expanded").text(GMS(M, n).map(col => "(" + col.join(",") + ")").join());
 }
 
 $(document).ready(function () {
