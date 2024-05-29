c24c197fd604e05ebb8f216d5bb6a4ebe33d471c
DaVinci103 <58339513+DaVinci103@users.noreply.github.com>
2024-05-29 19:52:40 +0200

    Update gms.js

--- gms.js
+++ gms.js
@@ -1,8 +1,3 @@
-/*
-Credits to Polymations for the expansion algorithm code:
-https://googology.fandom.com/wiki/User_blog:DaVinci103/Powerful_ordinal_notation_I_made?commentId=4400000000000044414&replyId=4400000000000130111
-*/
-
 function GMS(M, n) {
     let [c, b, a] = M[M.length - 1];
     
@@ -10,18 +5,14 @@ function GMS(M, n) {
     let B = M.slice(a, b);
     let C = M.slice(b, c);
     
-    let k, D;
-    if (b === c) {
-        k = c - a;
-        D = B;
-    } else {
-        k = c - a - 1;
-        D = [...B.slice(1), ...C];
-    }
+    let D;
+    if (b === c) D = B;
+    else D = [B[B.length - 1], ...C];
+    let k = D.length;
     
     De = i => D.map(col => col.map(x =>
         x < a ? x :
-        x === a ? k * i + b :
+        x < b - 1 ? k * i + x + b - a :
         k * i + x + k));
 
     return [...A, ...B, ...C, ...Array(n).fill().map((_, i) => De(i)).flat()];
